03a15a5bab8a18576703c950f2936725a7432dd7
松本悠里 <y_414snw@icloud.com>
2021-05-21 11:20:11 +0900

    kadai

--- ProfileController.php
+++ ProfileController.php
@@ -5,6 +5,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Profile;
 
+// 以下を追記
+use App\ProfileHistory;
+
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
     //
@@ -12,6 +17,7 @@ class ProfileController extends Controller
     {
     return view('admin.profile.create');
     }
+
     public function create(Request $request)
     {
        $this->validate($request, Profile::$rules);
@@ -24,6 +30,19 @@ class ProfileController extends Controller
        $profile->save();
 
        return redirect('admin/profile/create');
+    }
+
+    Public function index(Request $request)
+    {
+        $cond_name = $request->cond_name;
+        if ($cond_name != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('name', $cond_name)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
     }
 
      public function edit(Request $request)
@@ -42,7 +61,7 @@ class ProfileController extends Controller
          // Validationをかける
          $this->validate($request, Profile::$rules);
          // Profile Modelからデータを取得する
-         $profile = Profile::find($request->id);
+         $profile = Profile::find($request->input('id'));
          // 送信されてきたフォームデータを格納する
          $profile_form = $request->all();
 
@@ -50,6 +69,12 @@ class ProfileController extends Controller
          // 該当するデータを上書きして保存する
          $profile->fill($profile_form)->save();
 
-         return redirect('admin/news/');
+         // 以下を追記
+         $profile_history = new ProfileHistory;
+         $profile_history->profile_id = $profile->id;
+         $profile_history->edited_at = Carbon::now();
+         $profile_history->save();
+
+         return redirect('admin/profile/');
      }
 }
